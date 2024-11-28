f83cb4b6c586b296b7e91dfcb301865a101b8f5e
patricklafrance <pa.lafrance@gmail.com>
2024-11-28 14:03:59 -0500

    Removed @types/react-dom from libraries dependencies

--- libs/ext-lib/rslib.dev.ts
+++ libs/ext-lib/rslib.dev.ts
@@ -5,7 +5,8 @@ import { defineConfig } from "@rslib/core";
 export default defineConfig({
     lib: [{
         format: "esm",
-        syntax: "esnext"
+        syntax: "esnext",
+        dts: false
     }],
     source: {
         entry: {
